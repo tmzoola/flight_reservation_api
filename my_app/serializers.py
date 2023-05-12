b04fbca1188ea1431821beffd6db0c5b5a83e5fd
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-05-12 20:32:44 +0900

    Validations in serializers file

--- my_app/serializers.py
+++ my_app/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from .models import Flight,Passenger,Reservation
+import re
 
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+
+    def validate_flightNumber(self,flightNumber):
+        if(re.match("^[a-zA-Z0-9]*$",flightNumber)==None):
+            raise serializers.ValidationError("Invalid Flight Number. Make sure it is alpha numeric")
+        return flightNumber
+
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
